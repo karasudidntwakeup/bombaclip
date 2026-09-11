a2237267c66783ae572fcafa667459b68beb9bbb
karasu <hemdanoovic@gmail.com>
2026-09-12 01:22:02 +0300

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "bombaclip.karasu"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "3.1"
     }
 
     signingConfigs {
